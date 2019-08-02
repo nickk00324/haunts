dc9aed1f4c49fd3eabf132b658e13ae82a478551
nickk <nickkochornswasdi@gmail.com>
2019-08-01 22:07:55 -0700

    Added a scroll forever button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var people = document.querySelectorAll("li");
 var photo = document.getElementById("photo");
 
+var play_button = document.getElementById("play-button");
+
+play_button.addEventListener("click", play);
+var scroll_forever = false;
+
+
+
 people.forEach(function(person){
     person.addEventListener("mouseover", mouseOver);
     person.addEventListener("mouseleave", mouseOut);
@@ -20,4 +27,19 @@ function mouseOver(){
 
 function mouseOut(){
     photo.innerHTML = "";
+}
+
+function play(){
+    var scroll = document.querySelector(".scroll");
+    if(scroll_forever == false){
+        scroll.style = "animation: scroll_anim 30s linear infinite;";
+        play_button.style = "color: #ffb6b9;"
+        scroll_forever = true;
+    }else {
+        scroll.style = "animation: scroll_anim 30s linear 1;";
+        play_button.style = "color: #000";
+        scroll_forever = false;
+        console.log("clicked")
+    }
+
 }
